b28815aed9b861700fe68ee21e7f8975b8e1eaf9
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2026-03-01 01:32:56 -0400

    Agrega el llamado de la pantalla desde el App

---
+++ composeApp/src/commonMain/kotlin/com/aarondevs/kmpaccess/App.kt
@@ -0,0 +1,11 @@
+package com.aarondevs.kmpaccess
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.aarondevs.kmpaccess.presentation.screen.PermisosScreen
+
+@Composable
+@Preview
+fun App() {
+    PermisosScreen()
+}
